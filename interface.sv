a64fe5ccdb8c3b01af3095e4f8e92753fe6fff76
motheshashidhar <77180225+motheshashidhar@users.noreply.github.com>
2021-06-09 23:45:01 -0700

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,18 +1,18 @@
-interface InternalBus(input logic clk, rst);
+interface dmaIF(input logic clk, rst);
 
   wire  [1:0]channelNo;					
   logic [3:0]requestRegister;
   logic	isRead;
   logic [3:0]maskRegister;
   logic [7:0]statusRegister;
-  logic	transferMode;
+  logic	[1:0]transferMode;
   logic	RotatingPriority;
   logic FixedPriority;
-  logic	DREQ_Sense;
-  logic	DACK_Sense;
+  //logic	DREQ_Sense;
+  //logic	DACK_Sense;
   logic TC;							
   logic ldAck;						
-  logic Carry;						
+  logic carrypresent;						
   logic [3:0] dmaReq;					
   logic AddrGen;					
   logic ldUpperAddress;					
@@ -31,19 +31,23 @@ interface InternalBus(input logic clk, rst);
     ldTempRegister,
     ldTempAddr,
     dmaReq,
+    clk,
+    rst,
     output 		
-    Carry,			
+    carrypresent,			
     TC,
     isRead,
     transferMode,			
     RotatingPriority,
     FixedPriority,
-    DREQ_Sense,				
-    DACK_Sense,				
+    //DREQ_Sense,				
+    //DACK_Sense,				
     requestRegister,
     statusRegister,
     maskRegister								
   );
   
 endinterface
+
+
 
